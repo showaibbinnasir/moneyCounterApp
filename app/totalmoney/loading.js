665798089efe98f2b08ee817a30e2e1364a87ee4
showaibbinnasir <showaibnishad@gmail.com>
2025-07-29 08:45:22 -0500

    bug fixed

--- app/totalmoney/loading.js
+++ app/totalmoney/loading.js
@@ -2,8 +2,8 @@ import { RingLoader } from "react-spinners";
 
 const loading = () => {
     return (
-        <div className="h-screen w-screen flex justify-center items-center">
-            <div className="flex justify-center mt-[150px]"><RingLoader color="#ffffff" size={120} /></div>
+        <div className="flex mt-[150px] justify-center items-center">
+            <div className=""><RingLoader color="#ffffff" size={120} /></div>
         </div>
     );
 };
